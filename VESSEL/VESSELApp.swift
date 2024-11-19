ffb3198543c628d996da821c447bbd57413796b8
Hang Zhou <zhouhangseu@outlook.com>
2024-11-18 20:57:33 -0500

    Initial Commit

---
+++ VESSEL/VESSELApp.swift
@@ -0,0 +1,17 @@
+//
+//  VESSELApp.swift
+//  VESSEL
+//
+//  Created by Zhou Hang on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct VESSELApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
